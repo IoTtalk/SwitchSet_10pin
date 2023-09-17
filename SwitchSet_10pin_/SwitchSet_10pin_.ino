fd59fe685f3ea38c1a303ca72bcbf82b5e53a3a1
Jyneda <jyneda@gmail.com>
2023-09-17 16:15:51 +0800

    Add files via upload

--- SwitchSet_10pin_/SwitchSet_10pin_.ino
+++ SwitchSet_10pin_/SwitchSet_10pin_.ino
@@ -1,27 +1,28 @@
 #include <Bridge.h>
+#define defaultState LOW
 
 void setup() {
     pinMode(13,OUTPUT);
     pinMode(2,OUTPUT);
-    digitalWrite(2, HIGH);    
+    digitalWrite(2, defaultState);    
     pinMode(3,OUTPUT);
-    digitalWrite(3, HIGH);
+    digitalWrite(3, defaultState);
     pinMode(4,OUTPUT);
-    digitalWrite(4, HIGH);    
+    digitalWrite(4, defaultState);    
     pinMode(5,OUTPUT);    
-    digitalWrite(5, HIGH);
+    digitalWrite(5, defaultState);
     pinMode(6,OUTPUT);    
-    digitalWrite(6, HIGH);
+    digitalWrite(6, defaultState);
     pinMode(7,OUTPUT);
-    digitalWrite(7, HIGH);
+    digitalWrite(7, defaultState);
     pinMode(8,OUTPUT);    
-    digitalWrite(8, HIGH);
+    digitalWrite(8, defaultState);
     pinMode(9,OUTPUT);        
-    digitalWrite(9, HIGH);
+    digitalWrite(9, defaultState);
     pinMode(10,OUTPUT);        
-    digitalWrite(10, HIGH);
+    digitalWrite(10, defaultState);
     pinMode(11,OUTPUT);        
-    digitalWrite(11, HIGH);
+    digitalWrite(11, defaultState);
 
     Bridge.begin();   // Pins 0 and 1 should be avoided as they are used by the Bridge library.
 }
@@ -43,7 +44,9 @@ void loop() {
         if (strcmp(valueStr,"") != 0){
             valueInt = atoi(valueStr);                  
             //Serial.print(StrBuf); Serial.print(": "); Serial.println(valueStr);
-            if (valueInt > 0) valueInt=0; else valueInt=1;  // on/off reverse
+            if (defaultState == HIGH){
+                if (valueInt > 0) valueInt=0; else valueInt=1;  // on/off reverse
+            }
             digitalWrite(i+1, valueInt);        
             Bridge.put(StrBuf, "");
         }
